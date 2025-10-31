be27b8b867f9db40443006cb31b1b65c66254b87
j3rrryy <pavryab@mail.ru>
2025-11-01 01:26:31 +0300

    fix: renew_cert.sh

--- prod/renew_cert.sh
+++ prod/renew_cert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd ./school_464/
-docker compose -f docker-compose.cert.yml run --rm -d certbot renew
-docker compose -f docker-compose.prod.yml exec nginx nginx -s reload
+docker compose -f docker-compose.prod.yml stop nginx
+docker compose -f docker-compose.cert.yml run --rm certbot renew
+docker compose -f docker-compose.cert.yml down
+docker compose -f docker-compose.prod.yml start nginx
